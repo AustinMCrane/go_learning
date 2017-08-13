e8a7d1b5f552a7245c181f83d8f3c1138015409a
Austin M Crane <austin.crane@ennovar.wichita.edu>
2017-08-13 12:45:16 -0500

    issues with sql file.. missing commas

--- sql/seed.sql
+++ sql/seed.sql
@@ -17,10 +17,10 @@ VALUES
 
 INSERT INTO timeline (body, time, image)
 VALUES
-  ('We met', 'December 29, 2012', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-0/p206x206/62986_4720801491409_226450906_n.jpg?oh=f1819409882eb2a303be40a76c7525e8&oe=5A236C47')
-  ('We are in love', 'Febuary 2, 2012', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/485215_4859203191365_1005380069_n.jpg?oh=83caa7909f2c1ca14885a43ebf7644e4&oe=5A2F3923')
-  ('We meet the family', 'March 3, 2012', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-0/p206x206/969372_10151709309147182_1354214451_n.jpg?oh=61750b5a610e4b609b862a1b1ff980ca&oe=5A32F4CB')
-  ('Knocked her up', 'IDK', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/10629756_10100511497966232_4531025860085571582_n.jpg?oh=87a336db702df14a041071d0ef8a8602&oe=5A3078E1')
-  ('Hadley is born', 'March 5, 2015', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/10431543_10152923584118458_709911543536027567_n.jpg?oh=ebfb6e9bd9ab23b5e8fcf6522d7a6daf&oe=59F1D9F5')
-  ('We are engaged', 'IDK', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/18057912_10154674134963458_280330944505678958_n.jpg?oh=4234c0bff65ef909e55184d1cb672806&oe=5A20EACF')
+  ('We met', 'December 29, 2012', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-0/p206x206/62986_4720801491409_226450906_n.jpg?oh=f1819409882eb2a303be40a76c7525e8&oe=5A236C47'),
+  ('We are in love', 'Febuary 2, 2012', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/485215_4859203191365_1005380069_n.jpg?oh=83caa7909f2c1ca14885a43ebf7644e4&oe=5A2F3923'),
+  ('We meet the family', 'March 3, 2012', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-0/p206x206/969372_10151709309147182_1354214451_n.jpg?oh=61750b5a610e4b609b862a1b1ff980ca&oe=5A32F4CB'),
+  ('Knocked her up', 'IDK', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/10629756_10100511497966232_4531025860085571582_n.jpg?oh=87a336db702df14a041071d0ef8a8602&oe=5A3078E1'),
+  ('Hadley is born', 'March 5, 2015', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/10431543_10152923584118458_709911543536027567_n.jpg?oh=ebfb6e9bd9ab23b5e8fcf6522d7a6daf&oe=59F1D9F5'),
+  ('We are engaged', 'IDK', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/18057912_10154674134963458_280330944505678958_n.jpg?oh=4234c0bff65ef909e55184d1cb672806&oe=5A20EACF'),
   ('We are here today!', 'September 16, 2017', 'https://scontent.fmci1-1.fna.fbcdn.net/v/t1.0-9/19059264_10154823871378458_1487953269318551064_n.jpg?oh=07d66b274001220249369779a24a5172&oe=5A243662')
